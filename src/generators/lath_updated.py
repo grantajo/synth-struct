58a7c0c32da4f58a80f0f9129e6a11979ea7b472
grantajo <grantajo@iastate.edu>
2026-02-03 14:56:48 -0600

    Added a file to start thinking about implementing a CA method for creating lath microstructures

---
+++ src/generators/lath_updated.py
@@ -0,0 +1,13 @@
+# synth_struct/src/generators/lath_updated.py
+
+"""
+To be implemented:
+
+Use CA to simulate laths from a previously generated microstructure (Voronoi or columnar)
+
+Use the framework from: 
+- De Baere, et al. 2020. Materials Today Communications. doi: 10.1016/j.mtcomm.2020.101031
+    (has the CA framework for simulation)
+- Yang, et al. 2017. MSE A. doi: 10.1016/j.msea.2017.09.068 
+    (has the 12 orientation relationships between beta BCC to alpha HCP titanium)
+"""
